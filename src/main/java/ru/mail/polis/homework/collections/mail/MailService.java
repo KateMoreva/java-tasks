06e28ccd7c8ec12add6c7f98607b6d300fbcc5b3
Ekaterina Moreva <moreva.ei@edu.spbstu.ru>
2019-12-20 19:10:37 +0300

    HW1 files were moved to the new brunch.

--- src/main/java/ru/mail/polis/homework/collections/mail/MailService.java
+++ src/main/java/ru/mail/polis/homework/collections/mail/MailService.java
@@ -1,9 +1,7 @@
-package ru.mail.polis.homework.collections.mail;
 
-
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 /**
  * Нужно создать сервис, который умеет обрабатывать письма и зарплату.
@@ -12,42 +10,70 @@ import java.util.function.Consumer;
  *
  * В реализации нигде не должно быть классов Object и коллекций без типа. Используйте дженерики.
  */
-public class MailService implements Consumer {
+public class MailService implements Consumer<Mail> {
+
+    private Map<String, List<Mail>> mailBox;
+
+    private MailService(){
+        mailBox = new HashMap<>();
+    }
 
     /**
      * С помощью этого метода почтовый сервис обрабатывает письма и зарплаты
      * 1 балл
      */
     @Override
-    public void accept(Object o) {
-
+    public void accept(Mail email){
+        mailBox.computeIfAbsent(email.getRecipient(), (key) -> {
+            List<Mail> emailList = new ArrayList<>();
+            emailList.add(email);
+            return emailList;
+        });
     }
 
     /**
      * Метод возвращает мапу получатель -> все объекты которые пришли к этому получателю через данный почтовый сервис
      */
-    public Map<String, List> getMailBox() {
-        return null;
+    public Map<String, List<Mail>> getMailBox(){
+        return mailBox;
     }
 
     /**
      * Возвращает самого популярного отправителя
      */
-    public String getPopularSender() {
-        return null;
+    public String getPopularSender(){
+        return mailBox
+                .entrySet()
+                .stream()
+                .flatMap(element -> element.getValue().stream())
+                .collect(Collectors.groupingBy(Mail::getSender, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .max(Comparator.comparing(Map.Entry<String, Long>::getValue))
+                .get()
+                .getKey();
     }
 
     /**
      * Возвращает самого популярного получателя
      */
-    public String getPopularRecipient() {
-        return null;
+    public String getPopularRecipient(){
+        return mailBox
+                .entrySet()
+                .stream()
+                .flatMap(element -> element.getValue().stream())
+                .collect(Collectors.groupingBy(Mail::getRecipient, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .max(Comparator.comparing(Map.Entry<String, Long>::getValue))
+                .get()
+                .getKey();
     }
 
     /**
      * Метод должен заставить обработать service все mails.
      */
-    public static void process(MailService service, List mails) {
-
+    public static void process(MailService service, List mails){
+        mails.forEach(service);
     }
 }
--- src/main/java/ru/mail/polis/homework/collections/streams/WordFrequency.java
+++ src/main/java/ru/mail/polis/homework/collections/streams/WordFrequency.java
@@ -1,6 +1,10 @@
-package ru.mail.polis.homework.collections.streams;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -20,9 +24,16 @@ public class WordFrequency {
      * Задачу можно решить без единого условного оператора, только с помощью стримов.
      * Если будут использоваться условные операторы, то оценка максимальная оценка 2 балла.
      */
-    public static List<String> wordFrequency(Stream<String> lines) {
-        return null;
+    public static List<String> wordFrequency(Stream<String> lines){
+        return lines
+                .map(String::toLowerCase)
+                .flatMap(line -> Arrays.stream(line.split("[ .,!:-?;]+")))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Comparator.comparingLong(Map.Entry<String, Long>::getValue).reversed().thenComparing(Map.Entry::getKey))
+                .limit(10)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
-
 
 }
